da11c463ba0ee35e84b5641801af53bb2ea21a2b
Jan Brettschneider <jan.brettschneider@live.de>
2020-04-16 14:17:16 +0200

    Updated report

--- create_datasets.R
+++ create_datasets.R
@@ -1,5 +1,5 @@
 ################################
-# Create train set, validation set
+# Create train set, test set
 ################################
 
 # Load required packages if required
@@ -25,6 +25,13 @@ y_verification <- factor(verification[,1])
 
 x_images <- array(as.numeric(unlist(x)), dim=c(60000, 28, 28))
 x_verification_images <- array(as.numeric(unlist(x_verification)), dim=c(10000, 28, 28))
+
+x_mod <- modelling[1:50000,]
+x_test <- modelling[50001:60000,]
+x_sub <- x_images[1:50000,,]
+y_sub <- y[1:50000]
+x_test_sub <- x_images[50001:60000,,]
+y_test_sub <- y[50001:60000]
 
 fashion_cat = c('T-shirt/top',
                 'Trouser',
